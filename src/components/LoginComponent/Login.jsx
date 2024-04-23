252a2e170772cccbb70f70707a14dd12904f4619
Santanu Jana <santanuj.aeie2016@nsec.ac.in>
2024-04-24 03:35:16 +0530

    Changed CSS for opacity

--- src/components/LoginComponent/Login.jsx
+++ src/components/LoginComponent/Login.jsx
@@ -1,6 +1,6 @@
-import React from 'react'
+import React from "react";
 
-import Header from '../Header/Header';
+import Header from "../Header/Header";
 
 const Login = () => {
   return (
@@ -12,14 +12,24 @@ const Login = () => {
           alt="background"
         />
       </div>
-      <form className='absolute w-3/12 p-12 bg-black top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2'>
-        <h1 className='font-bold text-3xl py-4 text-white'>Sign In</h1>
-        <input type="email" placeholder="Email" className='w-full p-4 my-4'/>
-        <input type="password" placeholder="Password" className='w-full p-4 my-4'/>
-        <button className='w-full bg-red-600 py-4 text-white my-6'>Sign In</button>
+      <form className="absolute w-3/12 p-12 bg-black top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-opacity-75">
+        <h1 className="font-bold text-3xl py-4 text-white">Sign In</h1>
+        <input
+          type="email"
+          placeholder="Email"
+          className="w-full p-4 my-4 bg-gray-700 bg-opacity-80"
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          className="w-full p-4 my-4 bg-gray-700 bg-opacity-80"
+        />
+        <button className="w-full bg-red-600 py-4 text-white my-6">
+          Sign In
+        </button>
       </form>
     </div>
   );
-}
+};
 
 export default Login;
